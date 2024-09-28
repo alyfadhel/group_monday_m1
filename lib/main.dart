ceaa4141f825a5c2e287462ca1ad4dff95d13429
alyel <alyfadel67@gmail.com>
2024-09-28 21:35:17 +0300

    finish news app

--- lib/main.dart
+++ lib/main.dart
@@ -7,18 +7,15 @@ import 'package:group_monday_m1/core/shared/network/remote/dio_helper.dart';
 import 'package:group_monday_m1/core/shared/themes/themes.dart';
 import 'package:group_monday_m1/features/modules/counter/observer/observer.dart';
 
-
-
 void main() {
   DioHelper.init();
   Bloc.observer = MyBlocObserver();
   runApp(
-    // DevicePreview(
-    //   enabled: !kReleaseMode,
-    //   builder: (context) => const MyApp(), // Wrap your app
-    // ),
-    const MyApp()
-  );
+      // DevicePreview(
+      //   enabled: !kReleaseMode,
+      //   builder: (context) => const MyApp(), // Wrap your app
+      // ),
+      const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -27,17 +24,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (BuildContext context)=> NewsCubit()..getBusinessData()..getSportsData()..getScienceData(),
-      child: BlocConsumer<NewsCubit,NewsStates>(
-        listener: (context, state) {
-
-        },
+      create: (BuildContext context) => NewsCubit()
+        ..getBusinessData()
+        ..getSportsData()
+        ..getScienceData(),
+      child: BlocConsumer<NewsCubit, NewsStates>(
+        listener: (context, state) {},
         builder: (BuildContext context, state) {
           return MaterialApp(
             debugShowCheckedModeBanner: false,
-             theme: getLightMode(),
+            theme: getLightMode(),
             darkTheme: getDarkMode(),
-            themeMode: ThemeMode.light,
+            themeMode: ThemeMode.dark,
             home: const NewsLayout(),
           );
         },
